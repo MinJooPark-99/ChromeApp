a441465082f1ccfc3098a9dc11f94c093fbac2ae
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-03-07 01:21:14 +0900

    [feat] toDos saving

--- todo.js
+++ todo.js
@@ -4,16 +4,31 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
 
     delBtn.innerText = "❌";
-    const span = document.createElement("span");
     span.innerText = text;
     li.appendChild(span); //li안에 span 넣기
     li.appendChild(delBtn); //li안에 버튼넣기
+    li.id = newId;
     toDoList.appendChild(li);
+
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event) {
@@ -23,10 +38,14 @@ function handleSubmit(event) {
     toDoInput.value = "";
 }
 
-function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if (toDos === null) {
 
+function loadToDos() {
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function (toDo) {
+            paintToDo(toDo.text);
+        });
     }
 }
 
