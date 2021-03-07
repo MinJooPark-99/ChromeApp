51edbdf40eaa80423eca3f386bfcaee399311af1
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-03-07 18:38:14 +0900

    [feat] deleteToDos

--- todo.js
+++ todo.js
@@ -4,7 +4,19 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+
+    const cleanToDos = toDos.filter(function (toDo) {
+        return toDo.id !== parseInt(li.id); //parseInt => string을 int로 바꾸기
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
 
 function saveToDos() {
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -17,6 +29,7 @@ function paintToDo(text) {
     const newId = toDos.length + 1;
 
     delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(span); //li안에 span 넣기
     li.appendChild(delBtn); //li안에 버튼넣기
